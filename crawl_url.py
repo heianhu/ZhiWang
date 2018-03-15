dc86173126c3dbe603901a32a61f128dab7f2271
包袱里有抱负 <heianhu@live.com>
2018-03-15 23:28:27 +0800

    修改配置项

--- crawl_url.py
+++ crawl_url.py
@@ -4,6 +4,7 @@ __author__ = 'heianhu'
 
 from apps.crawl_data.crawl_summary import CrawlCnkiSummary
 
-crawl_cnki_summary = CrawlCnkiSummary(use_Chrome=True,)
+crawl_cnki_summary = CrawlCnkiSummary(use_Chrome=False,
+                                      executable_path='/Users/heianhu/phantomjs-2.1.1-macosx/bin/phantomjs')
 crawl_cnki_summary.crawl_periodicals_summary(mark=True)
 # crawl_cnki_summary.test()
