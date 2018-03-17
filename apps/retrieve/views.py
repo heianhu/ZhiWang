277f1663d64079da1db46888a4858731e949111b
包袱里有抱负 <heianhu@live.com>
2018-03-17 18:42:37 +0800

    修改分页功能

--- apps/retrieve/views.py
+++ apps/retrieve/views.py
@@ -3,9 +3,7 @@ from django.views.generic import View
 from crawl_data.models import Summary
 import jieba
 import jieba.posseg
-from django.core.paginator import Paginator
-from django.core.paginator import EmptyPage
-from django.core.paginator import PageNotAnInteger
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models.query import QuerySet
 
 
@@ -46,24 +44,30 @@ class Search(View):
         else:
             return render(request, 'index.html')
 
-        result_count = all_articles.count()
+        # result_count = all_articles.count()
+        #
+        # # 进行分页
+        # limit = 20  # 每页显示的记录数
+        # paginator = Paginator(all_articles, limit)  # 实例化一个分页对象
+        #
+        # page = request.GET.get('page')  # 获取页码
+        # try:
+        #     all_articles = paginator.page(page)  # 获取某页对应的记录
+        # except PageNotAnInteger:  # 如果页码不是个整数
+        #     all_articles = paginator.page(1)  # 取第一页的记录
+        # except EmptyPage:  # 如果页码太大，没有相应的记录
+        #     all_articles = paginator.page(paginator.num_pages)  # 取最后一页的记录
 
-        # 进行分页
-        limit = 20  # 每页显示的记录数
-        paginator = Paginator(all_articles, limit)  # 实例化一个分页对象
-
-        page = request.GET.get('page')  # 获取页码
+        # 分页功能
         try:
-            all_articles = paginator.page(page)  # 获取某页对应的记录
-        except PageNotAnInteger:  # 如果页码不是个整数
-            all_articles = paginator.page(1)  # 取第一页的记录
-        except EmptyPage:  # 如果页码太大，没有相应的记录
-            all_articles = paginator.page(paginator.num_pages)  # 取最后一页的记录
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+        p = Paginator(all_articles, 12, request=request)
+        articles = p.page(page)
 
         return render(request, 'result.html', {
-            'all_articles': all_articles,
-            'result_count': result_count,
+            'all_articles': articles,
             'search_type': search_type,
             'keywords': keywords
-
         })
