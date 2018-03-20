b8882b817a0f0621ff86ef0cbf731cb4448eb024
Eel 蒋 <eeel777jiang@gmail.com>
2018-03-21 00:51:55 +0800

    添加 搜索条件 searchfilter model

--- apps/retrieve/models.py
+++ apps/retrieve/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class SearchFilter(models.Model):
+    """
+    搜索条件
+    """
+    session_id = models.CharField(verbose_name='session_id', max_length=255, default='')
+    time = models.CharField(verbose_name='时间戳', default='', max_length=20)
+    filterPara = models.TextField(verbose_name='搜索条件', default='')
+
+
+    class Meta:
+        verbose_name = '搜索条件'
+        verbose_name_plural = verbose_name
