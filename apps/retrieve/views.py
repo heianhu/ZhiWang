541513a3fa9432f9cb9164f1f66d67c38ad868b0
包袱里有抱负 <heianhu@live.com>
2018-03-21 20:37:32 +0800

    修改文件结构

--- apps/retrieve/views.py
+++ apps/retrieve/views.py
@@ -10,10 +10,10 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, FileResponse, JsonResponse, HttpResponseRedirect
 from django.utils.http import urlquote
 from django.http import Http404
-from openpyxl import Workbook
 import json
 import time
 import datetime
+from retrieve.utils import write_to_excel, compress_excel
 
 
 # Create your views here.
@@ -28,6 +28,7 @@ class IndexView(View):
         #     pass
         return render(request, 'index.html', {
         })
+
 
 def get_query_set(search_filter):
     """
@@ -92,6 +93,7 @@ def get_query_set(search_filter):
 
     return all_articles
 
+
 class Search(View):
     def post(self, request):
         """
@@ -153,7 +155,6 @@ class Search(View):
 
         result_count = all_articles.count()
 
-
         # 分页功能
         try:
             page = request.GET.get('page', 1)
@@ -169,125 +170,6 @@ class Search(View):
             'result_count': result_count
         })
 
-
-
-def write_to_excel(getdetailinfo_id):
-    """
-    将指定id的文章写入excel
-    :param getdetailinfo_id:文章summary中的id号
-    :return: 生成的excel文件名(文章detail中的detail_id),失败时返回None
-    """
-    summary_id = int(getdetailinfo_id)
-    # 在Excel中插入数据，文件名为detail_id字段
-
-    fields = ['FN', 'VR', 'PT', 'AU', 'AF', 'BA', 'CA', 'GP', 'BE', 'TI', 'SO', 'SE', 'BS', 'LA', 'DT',
-              'CT', 'CY', 'CL', 'SP', 'HO', 'DE', 'ID', 'AB', 'C1', 'RP', 'EM', 'FU', 'FX', 'CR', 'NR',
-              'TC', 'Z9', 'PU', 'PI', 'PA', 'SN', 'BN', 'J9', 'JI', 'PD', 'PY', 'VL', 'IS', 'SI', 'PN',
-              'SU', 'BP', 'EP', 'AR', 'DI', 'D2', 'PG', 'P2', 'WC', 'SC', 'GA', 'UT', 'ER', 'EF']
-
-    values = {field: [i, ] for i, field in enumerate(fields)}
-    article_summary = Summary.objects.get(id=summary_id)
-    try:
-        article_detail = Detail.objects.get(id=article_summary.detail_id)
-    except Exception as e:
-        print ('部分summary没有detail',e)
-        return None
-
-    # 文件名
-    values['FN'].append(article_detail.detail_id)
-    # 作者
-    # values['AU'][1] = article_summary.authors
-    for author in article_summary.detail.authors.split():
-        if author.isdigit():
-            values['AU'].append(Authors.objects.filter(id=int(author))[0].authors_name)
-        else:
-            values['AU'].append(author)
-    # 标题
-    values['TI'].append(article_summary.title)
-    # 出版日期
-    values['PD'].append(article_summary.issuing_time.strftime('%Y/%m/%d'))
-    # issn号
-    values['SN'].append(Periodicals.objects.get(id=article_summary.source_id).issn_number)
-    # 出版物名称
-    values['SO'].append(Periodicals.objects.get(id=article_summary.source_id).name)
-    # 摘要
-    values['AB'].append(article_detail.detail_abstract)
-
-    try:
-        # 参考文献
-        article_reference = References.objects.get(id=article_detail.references_id)
-    except References.DoesNotExist:
-        # 处理没有文献情况
-        values['CR'].append('')
-
-    else:
-        str_refer = ['CBBD', 'CDFD', 'CJFQ', 'CMFD', 'CRLDENG', 'SSJD', 'CCND', 'CPFD']
-        refers = []  # 参考于各个期刊的id，每个期刊的id以列表形式存在 [[1,2],[3,4]]
-        for refer in str_refer:
-            refers.append(getattr(article_reference, refer).split())
-
-        all_refers = []  # 每一项是相应期刊的查询集
-        all_refers.append(ReferencesCBBD.objects.filter(id__in=refers[0]))
-        all_refers.append(ReferencesCDFD.objects.filter(id__in=refers[1]))
-        all_refers.append(ReferencesCJFQ.objects.filter(id__in=refers[2]))
-        all_refers.append(ReferencesCMFD.objects.filter(id__in=refers[3]))
-        all_refers.append(ReferencesCRLDENG.objects.filter(id__in=refers[4]))
-        all_refers.append(ReferencesSSJD.objects.filter(id__in=refers[5]))
-        all_refers.append(ReferencesCCND.objects.filter(id__in=refers[6]))
-        all_refers.append(ReferencesCPFD.objects.filter(id__in=refers[7]))
-
-        # # 对每一条结果取title值
-        # all_refers_title = (refer.title for queryset in all_refers for refer in queryset)
-        # # 将每个title分别填入
-        # for refers_title in all_refers_title:
-        #     values['CR'].append(refers_title)
-
-        # 对每一条结果逐条获取数据
-        all_refers = (refer for queryset in all_refers for refer in queryset)
-        # 将每个title分别填入
-        for refers in all_refers:
-            try:
-                temp_refers_info = (refers.title, refers.authors, refers.source, refers.issuing_time)
-            except AttributeError:
-                temp_refers_info = (refers.title, refers.info, refers.issuing_time)
-            values['CR'].append(temp_refers_info)
-
-    wb = Workbook()
-    sheet = wb.get_active_sheet()
-
-    # for k, [i, v] in values.items():
-    #     sheet.cell(row=i + 1, column=1).value = k
-    #     sheet.cell(row=i + 1, column=2).value = v
-    for key, (i, *all_value) in values.items():
-        sheet.cell(row=i + 1, column=1).value = key
-        for _, v in enumerate(all_value):
-            sheet.cell(row=i + 1, column=2 + _).value = ''.join(v)
-
-    # 保存并返回下载
-    filename = '{0}.xlsx'.format(article_detail.detail_id)
-    wb.save('media/' + filename)
-    return filename
-
-def compress_excel(ids):
-    """
-    将批量id的文章写入excel，然后压缩成一个zip文件
-    :param ids: summary文章id列表
-    :return: 生成的zip文件名(不包含路径)
-    """
-    files = []
-    for id in ids:
-        filename = write_to_excel(id)
-        files.append('media/{0}'.format(filename))
-
-    import zipfile
-    timestamp = str(time.time()).replace('.', '')
-    zip_name = '{0}.zip'.format(timestamp)
-    jungle_zip = zipfile.ZipFile('media/{0}'.format(zip_name), 'w')
-    for file in files:
-        jungle_zip.write(file, compress_type=zipfile.ZIP_DEFLATED)
-    jungle_zip.close()
-
-    return zip_name
 
 class GetDetailInfo(View):
     def get(self, request, getdetailinfo_id):
@@ -327,12 +209,14 @@ class DownloadSel(View):
         return JsonResponse({'status': 'success',
                              # 返回的data值将成为前端js请求下载压缩文件的url的参数
                              'data': zip_name.replace('.zip', ''),
-                            }, content_type='application/json')
+                             }, content_type='application/json')
+
 
 class DownloadZip(View):
     def get(self, request, zip_name):
         """
         处理下载zip文件请求
+        :param request:
         :param zip_name: 不带后缀的zip文件名
         :return: 文件流
         """
@@ -349,7 +233,8 @@ class DownloadAll(View):
     def get(self, request, query_id):
         """
         处理"下载全部"按钮
-        :param queryId: 由查询条件生成的queryid
+        :param request:
+        :param query_id: 由查询条件生成的queryid
         :return:
         """
         session_id = request.session.session_key
@@ -376,4 +261,3 @@ class DownloadAll(View):
         response['Content-Disposition'] = 'attachment;filename="{}"'.format(urlquote(zip_name))
 
         return response
-
