c45769286d8e68e7b27ac8ea9586fcd12521ff4a
包袱里有抱负 <heianhu@live.com>
2018-03-26 10:09:52 +0800

    修改测试样例

--- crawl_url.py
+++ crawl_url.py
@@ -7,5 +7,6 @@ from apps.crawl_data.crawl_summary import CrawlCnkiSummary
 crawl_cnki_summary = CrawlCnkiSummary(use_Chrome=True,
                                       # executable_path='/Users/heianhu/phantomjs-2.1.1-macosx/bin/phantomjs'
                                       )
-crawl_cnki_summary.crawl_periodicals_summary(mark=True, start_num=2)
-# crawl_cnki_summary.test()
+# crawl_cnki_summary.crawl_periodicals_summary(mark=True, start_num=33)
+# crawl_cnki_summary.crawl_periodicals_summary(issn_number='1004-6577')
+crawl_cnki_summary.test()
