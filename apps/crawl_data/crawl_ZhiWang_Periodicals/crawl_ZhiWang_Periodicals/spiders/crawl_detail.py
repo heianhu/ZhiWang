e9a9bc0327f5a590ed7cdb272974f45d3062cd43
包袱里有抱负 <heianhu@live.com>
2018-03-29 23:38:06 +0800

    修改文件结构，将提取数据的部分单独拿出来作为一个工具函数

--- apps/crawl_data/crawl_ZhiWang_Periodicals/crawl_ZhiWang_Periodicals/spiders/crawl_detail.py
+++ apps/crawl_data/crawl_ZhiWang_Periodicals/crawl_ZhiWang_Periodicals/spiders/crawl_detail.py
@@ -9,6 +9,7 @@ from crawl_data.crawl_ZhiWang_Periodicals.crawl_ZhiWang_Periodicals.items import
     ReferencesSSJDItem, ReferencesCRLDENGItem, ReferencesItem, ReferencesCCNDItem, ReferencesCPFDItem
 from crawl_data.models import ReferencesCJFQ, ReferencesCMFD, ReferencesCDFD, ReferencesCBBD, ReferencesSSJD, \
     ReferencesCRLDENG, References, ReferencesCCND, ReferencesCPFD
+from .SelectData import select_detail
 
 
 class CrawlDetailSpider(scrapy.Spider):
@@ -53,41 +54,9 @@ class CrawlDetailSpider(scrapy.Spider):
         :return:
         """
         summary = response.meta.get('summary')
-        try:
-            # 有英文名字，即新版本
-            date = response.css(".sourinfo a::text").extract()[2].split()[0]  # 文章的年月 Detail.detail_date
-        except IndexError:
-            try:
-                # 没有英文名字，即旧版本
-                date = response.css(".sourinfo a::text").extract()[1].split()[0]
-            except IndexError:
-                date = response.css(".sourinfo p::text").extract()[0].split()[0]
-
-        paper_id = re.search('filename=((.*?))&', response.url).group(1)  # 文章ID Detail.detail_id
-
-        authors_info = response.css('.author a::attr(onclick)').re('(\(.*\))')
-        authors_dic = {}  # id:name     作者信息 Detail.authors     Authors.authors_id:Authors.authors_name
-        for i in authors_info:
-            author_name = i.split("','")[1]
-            author_id = i.split("','")[-1].split("'")[0]
-            authors_dic[author_id] = author_name
-
-        # orgs_info = response.css('.orgn a').re('(\(.*\))')
-        orgs_info = response.css('.orgn a::attr(onclick)').re(r'\((.*)\)')
-        orgs_dic = {}  # id:name   单位信息 Detail.organizations    Organization.organization_id:Organization.organization_name
-        for i in orgs_info:
-            org_name = i.split("','")[1]
-            org_id = i.split("','")[-1].split("'")[0]
-            orgs_dic[org_id] = org_name
-
-        abstract = response.xpath('//span[@id="ChDivSummary"]/text()').extract_first()  # 概要 Detail.detail_abstract
-
-        keyword = response.xpath('//div[@class="wxBaseinfo"]').re(r'\(\'kw\'.*\)', )
-        keywords = ""  # 关键字 Detail.detail_keywords
-        for i in keyword:
-            keywords += i.split("','")[1] + ' '
 
         detail_item = DetailItem()
+        paper_id, keywords, abstract, date, authors_dic, orgs_dic = select_detail(response=response)
         detail_item['detail_id'] = paper_id
         detail_item['detail_keywords'] = keywords
         detail_item['detail_abstract'] = abstract
@@ -478,7 +447,8 @@ class CrawlDetailSpider(scrapy.Spider):
             # print('CPFD_list:', CPFD_list)
 
             if len(
-                    CJFQ_list + CDFD_list + CMFD_list + CBBD_list + SSJD_list + CRLDENG_list + CCND_list + CPFD_list) == 0:
+                    CJFQ_list + CDFD_list + CMFD_list + CBBD_list + SSJD_list + CRLDENG_list + CCND_list + CPFD_list
+            ) == 0:
                 references = References.objects.filter(id=76438)[0]
             else:
                 references = References()
