d481e82623246f0825f340a1253341f73cd9107b
Eel 蒋 <eeel777jiang@gmail.com>
2018-05-03 16:42:24 +0800

    修改正则表达式

--- apps/crawl_cnki/crawl_Cnki_Periodicals/crawl_Cnki_Periodicals/RegularExpressions.py
+++ apps/crawl_cnki/crawl_Cnki_Periodicals/crawl_Cnki_Periodicals/RegularExpressions.py
@@ -9,7 +9,6 @@ RE_REFER_URL = re.compile(r'href=\"(.*?)\">')
 RE_REFER_TITLE = re.compile(r'<a(.*?)>(.*?)</a>')
 RE_ISSUING_TIME = re.compile(r'([\d]+-[\d]+-[\d]+)')
 RE_remove_space = re.compile('\n|\r|  |&amp|;nbsp')
-RE_remove_space_2 = re.compile('\n|\r|  ')
 RE_clean_abstract = re.compile(r'ChDivSummary">(.*?)</span>', )
 RE_clean_keyword = re.compile(r'TurnPageToKnet\(\'kw\',\'(.*?)\',\'[\d]*\'\)', )
 RE_clean_fund = re.compile(r'TurnPageToKnet\(\'fu\',\'(.*?)\',\'[\d]*\'\)', )
@@ -18,11 +17,15 @@ RE_clean_CJFQ = re.compile(r'href="(.*?)">(.*?)</a>(.*?)<a(.*?)>(.*?)</a>(.*?)<a
 RE_clean_CJFQ_else = re.compile(r'(.*?)\.(.*?)\.(.*?)\.(.*?)')
 RE_clean_CDFD = re.compile(r'href="(.*?)">(.*?)</a>(.*?)<a(.*?)>(.*?)</a>(.*?)([\d]{4})',)
 RE_clean_CMFD = re.compile(r'href="(.*?)">(.*?)</a>(.*?)<a(.*?)>(.*?)</a>(.*?)([\d]{4})', )
-RE_clean_CBBD = re.compile(r'(.*?)\.(.*?),(.*?),.*?([\d]{4})', )
+# RE_clean_CBBD = re.compile(r'(.*?)\.(.*?),(.*?),.*?([\d]{4})', )
+RE_clean_CBBD = re.compile(r'(.*?)\.(.*?),(.*?),([\d\s]*)', ) # 已修改一次
+
 RE_clean_SSJD = re.compile(r'href="([\s\S]*?)">(.*?)</a>([\s\S]*?)([\d\(\)]+)', )
 RE_clean_CRLDENG = re.compile(r'<a([\s\S]*?)>(.*?)</a>([\s\S]*)', )
 RE_clean_CRLDENG_else = re.compile(r'([\s\S]*)\.[\s]?([\d]*)', )
-RE_clean_CCND = re.compile(r'<a([\s\S]*?)>(.*?)</a>([\s\S]*)\.(.*?)\.([\d]{4}\([\d\w]+\))', )  # 已修改2次
+# RE_clean_CCND = re.compile(r'<a([\s\S]*?)>(.*?)</a>([\s\S]*)\.(.*?)\.([\d]{4}\([\d\w]+\))', )  # 已修改2次
+RE_clean_CCND = re.compile(r'<a([\s\S]*?)>(.*?)</a>([\s\S]*)\.(.*?)\.(.*)', )  # 已修改3次
+
 RE_split_word = re.compile(
     r'(QueryID=[a-zA-Z0-9.]&|CurRec=\d*&|DbCode=[a-zA-Z]*&|urlid=[a-zA-Z0-9.]*&|yx=[a-zA-Z]*)',
     flags=re.I
