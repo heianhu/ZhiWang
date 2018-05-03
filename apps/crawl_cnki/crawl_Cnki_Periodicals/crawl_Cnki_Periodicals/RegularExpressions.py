be6348dd2a0c41435e308576df7c92c0ce555d35
Eel 蒋 <eeel777jiang@gmail.com>
2018-05-03 14:10:35 +0800

    修改正则表达式

--- apps/crawl_cnki/crawl_Cnki_Periodicals/crawl_Cnki_Periodicals/RegularExpressions.py
+++ apps/crawl_cnki/crawl_Cnki_Periodicals/crawl_Cnki_Periodicals/RegularExpressions.py
@@ -4,7 +4,7 @@ __author__ = 'heianhu'
 import re
 
 RE_FILENAME = re.compile(r'filename=((.*?))&')
-RE_AUTHORS = re.compile('TurnPageToKnet\((.*?)\)">')
+RE_AUTHORS = re.compile('TurnPageToKnet\((.*?)\)[;]?">') #已修改1次(增加分号)
 RE_REFER_URL = re.compile(r'href=\"(.*?)\">')
 RE_REFER_TITLE = re.compile(r'<a(.*?)>(.*?)</a>')
 RE_ISSUING_TIME = re.compile(r'([\d]+-[\d]+-[\d]+)')
@@ -22,7 +22,7 @@ RE_clean_CBBD = re.compile(r'(.*?)\.(.*?),(.*?),.*?([\d]{4})', )
 RE_clean_SSJD = re.compile(r'href="([\s\S]*?)">(.*?)</a>([\s\S]*?)([\d\(\)]+)', )
 RE_clean_CRLDENG = re.compile(r'<a([\s\S]*?)>(.*?)</a>([\s\S]*)', )
 RE_clean_CRLDENG_else = re.compile(r'([\s\S]*)\.[\s]?([\d]*)', )
-RE_clean_CCND = re.compile(r'<a([\s\S]*?)>(.*?)</a>([\s\S]*)\.(.*?)\.([\d]{4}\([\d]+\))', )  # 已修改1次
+RE_clean_CCND = re.compile(r'<a([\s\S]*?)>(.*?)</a>([\s\S]*)\.(.*?)\.([\d]{4}\([\d\w]+\))', )  # 已修改2次
 RE_split_word = re.compile(
     r'(QueryID=[a-zA-Z0-9.]&|CurRec=\d*&|DbCode=[a-zA-Z]*&|urlid=[a-zA-Z0-9.]*&|yx=[a-zA-Z]*)',
     flags=re.I
